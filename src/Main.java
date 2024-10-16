54aa9fbd296301d79e4ee2a45280cd17679c1a1b
Максим <166473866+Macks3891@users.noreply.github.com>
2024-10-16 19:46:26 +0500

    hw 28 - 1 Commit

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+                char[] chars = {'A', 'S', 'W', 'n', 'b'};
+
         System.out.println(Methods.square(12));
         System.out.println(Methods.square(70));
         System.out.println(Methods.square(3));
-        System.out.println(Methods.printFullmame("Macksim", " Zhirov"));
+        System.out.println(Methods.printFullName("Macksim", " Zhirov"));
         String[] text = new String[]{"Word", "Word Java", "Word Java World", "W"};
         System.out.println(Arrays.toString(Methods.cornerValuesArray(text)));
+        System.out.println(Arrays.toString(Methods.cornerValuesArray(arr)));
+        System.out.println(Methods.cornerValuesArray(chars));
     }
 }
--- src/Methods.java
+++ src/Methods.java
@@ -1,25 +1,94 @@
-import java.util.Scanner;
-
+/**
+ * Класс Methods содержит статические методы для выполнения различных операций.
+ */
 public class Methods {
-    static int square(int number) {
+    private static String min;
+    private static String max;
+
+    /**
+     * Вычисляет квадрат переданного целого числа
+     * number целое число, квадрат которого нужно вычислить
+     * квадрат переданного числа
+     */
+    public static int square(int number) {
         return (int) Math.pow(number, 2);
     }
 
-    static String printFullmame(String name, String lastName) {
+    /**
+     * Возвращает полное имя, объединяя имя и фамилию.
+     * name имя
+     * lastName фамилия
+     * полное имя в формате "Имя Фамилия"
+     */
+    public static String printFullName(String name, String lastName) {
         return name + " " + lastName;
     }
 
-    static String[] cornerValuesArray(String[] arr) {
-        String shortWord = arr[0];
-        String longWord = arr[0];
-        for (String element : arr) {
-            if (element.length() < shortWord.length()) {
-                shortWord = element;
+    /**
+     * Находит самую короткую и самую длинную строку из переданных строк.
+     *  arr переменное количество строк
+     *  массив из двух строк: самая короткая и самая длинная строка
+     */
+    public static String[] cornerValuesArray (String... arr) {
+        String min = arr[0];
+        String max = arr[0];
+        int minn = arr[0].length();
+        int maxx = arr[0].length();
+        for (int i = 1; i < arr.length; i++){
+            if (minn > arr[i].length()) {
+                minn = arr[i].length();
+                min = arr[i];
             }
-            if (element.length() > longWord.length()) {
-                longWord = element;
+            else if (maxx < arr[i].length()) {
+                maxx = arr[i].length();
+                max = arr[i];
             }
         }
-        return new String[]{shortWord, longWord};
+
+        return new String[] {min, max};
+    }
+    /**
+     * Находит минимальное и максимальное число из переданных целых чисел.
+     * arr переменное количество целых чисел
+     * массив из двух чисел: минимальное и максимальное число
+     */
+    public static int[] cornerValuesArray(int... arr) {
+        if (arr.length == 0) {
+            return new int[] {0, 0};
+        }
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int num = 1; num < arr.length; num++) {
+            if (arr[num] < min) {
+                min = arr[num];
+            }
+            if (arr[num] > max) {
+                max = arr[num];
+            }
+        }
+        return new int[]{min, max};
+    }
+    /**
+     * Находит минимальный и максимальный символ из переданных символов.
+     * arr переменное количество символов
+     * массив из двух символов: минимальный и максимальный символ
+     */
+    public static char[] cornerValuesArray(char... arr) {
+        if (arr.length == 0) {
+            return new char[] {0, 0};
+        }
+        char min = arr[0];
+        char max = arr[0];
+
+        for (char i =1; i < arr.length; i++) {
+            if (i < min) {
+                min = i;
+            }
+            if (i > max) {
+                max = i;
+            }
+        }
+        return new char[] {min, max};
     }
 }
